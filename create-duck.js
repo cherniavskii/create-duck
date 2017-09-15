5ed85e4e977b2a29d5b0401cc91e4a636accf409
Andrzej <andrew.cherniavskii@gmail.com>
2017-09-15 20:20:22 +0200

    use prompt instead of commander

--- create-duck.js
+++ create-duck.js
@@ -1,16 +1,26 @@
-const program = require("commander");
+const prompt = require("prompt");
 const path = require("path");
 const fs = require("fs");
 const duckContent = require("./duck-content");
 
-const script = (duckName) => {
-	const filename = path.resolve(duckName + ".js");
-	fs.writeSync(fs.openSync(filename, "w"), duckContent(duckName));
-	console.log(`Created ${duckName} duck`);
+const schema = {
+	properties: {
+		name: {
+			description: 'Duck name',
+			type: 'string',
+			message: 'Duck name is required',
+			required: true,
+			default: 'default',
+		}
+	}
 };
 
-program
-	.version('0.1.0')
-	.arguments('<name>')
-	.action(script)
-	.parse(process.argv);
+prompt.message = " ";
+prompt.delimiter = ">";
+
+prompt.get(schema, (err, result) => {
+	const { name } = result;
+	const filename = path.resolve(name + ".js");
+	fs.writeSync(fs.openSync(filename, "w"), duckContent(name));
+	console.log(`Created ${name} duck`);
+});
