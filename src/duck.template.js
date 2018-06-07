d5a18b32b600c6c3315c031a4a06d12f5c8db19f
Andrew <andrew.cherniavskii@gmail.com>
2018-06-07 21:42:13 +0200

    reorganize duck template

--- src/duck.template.js
+++ src/duck.template.js
@@ -5,24 +5,26 @@ module.exports = (names) => {
 	return (
 		`import { createLogic } from 'redux-logic';
 
+const prefix = '${name}/';
+
 /*
  * TYPES
  */
-
-const prefix = '${name}/';
 
 const FETCH_${NAME} = \`\${prefix}FETCH_${NAME}\`;
 const FETCH_${NAME}_SUCCESS = \`\${prefix}FETCH_${NAME}_SUCCESS\`;
 const FETCH_${NAME}_CANCEL = \`\${prefix}FETCH_${NAME}_CANCEL\`;
 
+export const types = {
+  FETCH_${NAME},
+  FETCH_${NAME}_SUCCESS,
+  FETCH_${NAME}_CANCEL,
+};
+
+
 /*
  * ACTIONS
  */
-
-const fetch${Name}Success = data => ({
-  type: FETCH_${NAME}_SUCCESS,
-  data,
-});
 
 const fetch${Name} = options => ({
   type: FETCH_${NAME},
@@ -33,9 +35,51 @@ const fetch${Name} = options => ({
   },
 });
 
+const fetch${Name}Success = data => ({
+  type: FETCH_${NAME}_SUCCESS,
+  data,
+});
+
 const fetch${Name}Cancel = () => ({
   type: FETCH_${NAME}_CANCEL,
 });
+
+export const actions = {
+  fetch${Name},
+  fetch${Name}Success,
+  fetch${Name}Cancel,
+};
+
+
+/*
+ * SELECTORS
+ */
+
+const get${Name} = state => state;
+
+export const selectors = {
+  get${Name},
+};
+
+
+/*
+ * LOGIC
+ */
+
+const fetch${Name}Logic = createLogic({
+  type: FETCH_${NAME},
+  cancelType: [FETCH_${NAME}_CANCEL],
+  latest: true,
+  process({ action: { payload }, httpClient, cancelled$ }) {
+    return httpClient.cancellable(payload, cancelled$)
+      .then(({ data }) => fetch${Name}Success(data));
+  },
+});
+
+export const logic = {
+  fetch${Name}Logic,
+};
+
 
 /*
  * REDUCER
@@ -53,50 +97,7 @@ const reducer = (state = initialState, action) => {
   return (actions[action.type] && actions[action.type]()) || state;
 };
 
-/*
- * LOGIC
- */
-
-const fetch${Name}Logic = createLogic({
-  type: FETCH_${NAME},
-  cancelType: [FETCH_${NAME}_CANCEL],
-  latest: true,
-  process({ action: { payload }, httpClient, cancelled$ }) {
-    return httpClient.cancellable(payload, cancelled$)
-      .then(({ data }) => fetch${Name}Success(data));
-  },
-});
-
-/*
- * SELECTORS
- */
-
-const get${Name} = state => state;
-
-/*
- * EXPORTS
- */
-
 export default reducer;
-
-export const actions = {
-  fetch${Name},
-  fetch${Name}Success,
-  fetch${Name}Cancel,
-};
-
-export const types = {
-  FETCH_${NAME},
-  FETCH_${NAME}_SUCCESS,
-  FETCH_${NAME}_CANCEL,
-};
-
-export const logic = {
-  fetch${Name}Logic,
-};
-
-export const selectors = {
-  get${Name},
-};`
+`
 );
 };
